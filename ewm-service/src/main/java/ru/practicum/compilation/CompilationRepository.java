8414c676ff99e6ab2534e82ed194d51002ebbcee
GodynaEvgenia <evgeniagodyna@yandex.ru>
2025-10-09 23:03:58 +0300

    Основной сервис. Подборки событий, сохранение  в цикле заменено на saveAll

--- ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
+++ ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.compilation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
     Page<Compilation> findByPinned(Boolean pinned, Pageable pageable);
